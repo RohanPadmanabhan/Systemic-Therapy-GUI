c13c556ff567ccf5c74d32c93c6f67958138a147
RohanPadmanabhan <rohan@rohan-Latitude-E5450.com>
2017-09-15 16:18:17 +0100

    Add normalization of biomarkers.

--- oSCORADPred.m
+++ oSCORADPred.m
@@ -2,14 +2,15 @@ function [weekPredictions, imprStatus] = oSCORADPred(biomarkers, isAza, initScor
 
 %% Calculate rest
 coeffs = [2.92, 2.92, 2.12, -2.99, -4.25];
-rest = coeffs * biomarkers;
+means = [1.248; 0.938; 0.809; 2.076; -0.093];
+stdDevs = [0.713; 0.210; 0.326; 0.194; 0.971];
+rest = calculateRest(coeffs, biomarkers, means, stdDevs);
 
 %% Calculate improvement status
 mcid = 9;
 imprStatus = improvementStatus(rest, mcid);
 
 %% Calculate prediction at different time points
-
 if isAza
     abcd = [7.84, 0.14, 0.83, -0.02];
 else
