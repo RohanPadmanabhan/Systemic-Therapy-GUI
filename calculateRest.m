e42504e6d2e5c5679bc7483723c1d81acb172cfb
RohanPadmanabhan <rohan@rohan-Latitude-E5450.com>
2017-09-20 16:07:32 +0100

    Replace logged values with unlogged ones.

--- calculateRest.m
+++ calculateRest.m
@@ -1,5 +1,6 @@
 function [rest] = calculateRest(coeffs, biomarkers, means, stdDevs)
 
+biomarkers = log10(biomarkers);
 normBiomarkers = (biomarkers - means) ./ stdDevs;
 rest = coeffs * normBiomarkers;
 
